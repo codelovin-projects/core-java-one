40caff1b96c589c53c5b0afdfd0fc6827f6122b6
coderport <35043387+coderport@users.noreply.github.com>
2018-12-09 07:20:56 +0530

    Add files via upload

---
+++ src/main/java/com/coderport/corejavaone/CorejavaoneApplication.java
@@ -0,0 +1,12 @@
+package com.coderport.corejavaone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorejavaoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorejavaoneApplication.class, args);
+	}
+}
